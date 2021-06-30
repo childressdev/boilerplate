8e306189ac636ce65bb3d251b26728ad4a864a87
childressdev <jcampbell@childressagency.com>
2021-06-30 18:30:01 -0400

    added

---
+++ wp-theme-files/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<main id="main">
+  <div class="container">
+    <h2>Nothing Here</h2>
+    <p>Sorry, the page you were looking for could not be found.</p>
+  </div>
+</main>
+<?php get_footer();
