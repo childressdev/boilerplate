222822f316aa65215ed2f66c20fa3a5440aea22a
childressdev <jcampbell@childressagency.com>
2021-07-12 11:52:27 -0400

    include dart-sass compiler for gulp-sass 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
