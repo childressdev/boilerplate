0a334bd4346c73275f6c99d0f4e4edd3944268de
childressdev <jcampbell@childressagency.com>
2023-04-14 16:01:12 -0400

    added default content

--- wp-theme-files/front-page.php
+++ wp-theme-files/front-page.php
@@ -4,5 +4,18 @@
  */
 
 get_header(); ?>
+<main id="main">
+  <div class="container-fluid">
+    <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post(); 
+          the_content();
 
+          get_template_part('partials/content', 'edit_link');
+        }
+      }
+    ?>
+  </div>
+</main>
 <?php get_footer(); ?>
